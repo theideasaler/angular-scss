ac13b697d24d9ef7eb0256039fd899c1e1511419
DESKTOP-1KE2VOK\19545 <theideasaler@gmail.com>
2020-07-26 17:32:50 +1000

    rename .css files to .scss

--- src/app/scss-sample/scss-sample.component.ts
+++ src/app/scss-sample/scss-sample.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-scss-sample',
   templateUrl: './scss-sample.component.html',
-  styleUrls: ['./scss-sample.component.css']
+  styleUrls: ['./scss-sample.component.scss']
 })
 export class ScssSampleComponent implements OnInit {
 
